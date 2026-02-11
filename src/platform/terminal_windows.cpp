baca97c991630781b8e2093047428bf2601df6f1
corfaction <corfaction@gmail.com>
2026-02-11 23:07:37 +0500

    fix(terminal): terminal_windows.hpp implementation not working - Add setchar() and flushbuffer() methods - Add constructor for initializing private fields

--- src/platform/terminal_windows.cpp
+++ src/platform/terminal_windows.cpp
@@ -2,25 +2,48 @@
 #include <windows.h>
 
 class Terminal::Impl {
+private:
+    HANDLE hConsole;
 public:
+
+    Impl() {
+        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        DWORD mode = 0;
+        GetConsoleMode(hConsole, &mode);
+        mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        SetConsoleMode(hConsole, mode);
+    }
+
     void getWindowSize(size_t& width, size_t& height) const {
         CONSOLE_SCREEN_BUFFER_INFO csbi;
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
         width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-        height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-        
+        height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1; 
     }
 
     float getAspectRatio() const {
         CONSOLE_FONT_INFOEX info{};
         info.cbSize = sizeof(info);
 
-        getCurrentConsoleFontEx(
+        GetCurrentConsoleFontEx(
             GetStdHandle(STD_OUTPUT_HANDLE),
             FALSE,
             &info
         );
+        return static_cast<float>(info.dwFontSize.Y) / static_cast<float>(info.dwFontSize.X);
     }
 
-    return static_cast<float>(info.dwFontSize.Y) / static_cast<float>(info.dwFontSize.X);
+    void setChar(size_t x, size_t y, char c) {
+        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        COORD coord;
+        coord.X = x;
+        coord.Y = y; 
+
+        std::cout << "\033[" + std::to_string(y + 1) + ';' + std::to_string(x + 1) + 'H' + c;
+    }
+
+    void flushBuffer() {
+        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleCursorPosition(hConsole, {0, 0});
+    }
 };
